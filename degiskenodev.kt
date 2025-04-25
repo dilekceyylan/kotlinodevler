36cd797e20862bf0cb8ab9865d579988ba3d1558
dilekceyylan <dilekceyylan@gmail.com>
2025-04-25 21:51:55 +0300

    Add files via upload

---
+++ degiskenodev.kt
@@ -0,0 +1,66 @@
+package com.example.day1.day1
+
+fun main() {
+
+    var sehir = "Afyon"
+    var ulke = "Turkiye"
+    var telefon = "+90 532 000 0000"
+    var postaKodu = "03000"
+    var email = "dilekceyylan@gmail.com"
+    var meslek = "Yazilimci"
+    var stokMiktari = 200
+    var musteriAdi = "Melek Ceylan"
+    var bakiye = 6000
+    var dogumGunu = "1992-25-05"
+    var maas = 30000
+    var medeniDurum = "Bekar"
+    var urunYorumu = "Kullanışlı"
+
+    var odemeTarihi = "2025-04-23"
+    var odeme = 900
+    var siparisAdeti = 2
+    var arabaModeli = "Clio"
+    var kitapAdi = "Kotlin Temelleri"
+    var yayinlanmaTarihi = "2022-09-15"
+    var indirimMiktari = 20.0
+    var odaSayisi = 4
+    var enlem = 38.75
+    var boylam = 40.5
+    var urunAdi = "Klavye"
+    var yemekFiyati = 95.0
+
+    var marka = "Monster"
+    var muzikAdi = "Kill the Pain"
+    var videoSuresi = "2:26"
+    var urunPuani = 4.9
+    var resimAdi = "urun1.jpg"
+    var dosyaFormati = "PNG"
+    var renk = "Kirmizi"
+    var renkKodu = "#FF0000"
+    var telefonModeli = "Samsung S23"
+    var ekranBoyutu = 6.5
+    var agirlik = 0.5
+
+    var ulusalGun = "23 Nisan"
+    var tatilGunu = "1 Ocak"
+    var rezervasyonTarihi = "2025-05-09"
+    var sokakAdi = "Cumhuriyet Sokak"
+    var otobusHatti = "12"
+    var kalanDakika = 11
+    var takipKodu = "KLMN11"
+    var kuponSuresi = "24 Saat"
+    var kuponKodu = "INDIRIM25"
+    var faturaAdresi = "Elmacık Sokak 56 Caddesi 56.Daire"
+}
+
+
+
+
+
+
+
+
+
+
+
+}
